bac5213d317dfec720a07c7038c77db349d6b092
Jaroslaw Mejer <jaroslaw.mejer@gmail.com>
2020-11-03 19:04:23 +0100

    learn: controlling flow of execution

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,13 +8,33 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Jarek Grade Book") ;
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+            while(true){
+                System.Console.WriteLine("Please add a grade or type q if You are done");
+                var input = Console.ReadLine();
+                if(input != "q"){
+                    try
+                    {
+                        var parsedGrade = double.Parse(input);
+                        book.AddGrade(parsedGrade);
+                    } 
+                    catch(Exception ex)
+                    {
+                        System.Console.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        // executes code even if there is an error
+                    }
+                    
+                } else{
+                    break;
+                }
+            }
             var stats = book.GetStatistics();
             System.Console.WriteLine($"The lowest grade is: {stats.Low}");
             System.Console.WriteLine($"The highest grade is: {stats.High}");
             System.Console.WriteLine($"The average: {stats.Average}");
+            System.Console.WriteLine($"The letter grade is: {stats.Letter}");
         }
     }
 }
